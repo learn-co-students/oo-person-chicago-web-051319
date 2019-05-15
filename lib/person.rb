20811cec8489ae6ad0937b6fea1992de9b668570
Emily R <emilyroulund@gmail.com>
2019-05-15 20:56:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,28 @@
 class Person
-  attr_accessor
-  attr_reader
+  attr_accessor :dollars, :happiness_points, :hygiene_points  
+  attr_reader :name
   attr_writer
   
-  def 
+  
+  
+  def get_paid (salary)
+    bank account += salary 
+    return "all about the benjamins"
+  end
+  
+  def take_bath
   end 
+  
+  def call_friend 
+  end 
+  
+  def start_conversation
+  end 
+  
+  def clean? 
+  end 
+  
+  def happy?
+  end 
+  
 end 
